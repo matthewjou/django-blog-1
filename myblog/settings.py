4874b6c48eadad6c552ffb6b1a85891423998d71
Jenna <jwz5299@uw.edu>
2024-12-21 19:30:36 -0800

    refactored structure

--- myblog/settings.py
+++ myblog/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'myblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'myblog/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
