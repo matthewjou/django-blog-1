074bcaab95b34be8c4d93a03381d9bd2fd898c80
Matthew Jou <joum@Matthews-Air.lan>
2024-12-23 12:33:38 -0800

    update blogging templates, models, and views, also update myblog template base.html

--- blogging/models.py
+++ blogging/models.py
@@ -8,11 +8,12 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
-    published_date = models.DateTimeField(blank=True,null=True)
-    
+    published_date = models.DateTimeField(blank=True, null=True)
+
     def __str__(self):
         return self.title
     
+
 class Category(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(blank=True)
@@ -20,3 +21,6 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Categories' 
--- blogging/views.py
+++ blogging/views.py
@@ -16,18 +16,14 @@ from blogging.models import Post
 def list_view(request):
     published = Post.objects.exclude(published_date__exact=None)
     posts = published.order_by('-published_date')
-    template = loader.get_template('blogging/list.html')
     context = {'posts': posts}
-    body = template.render(context)
     return render(request, 'blogging/list.html', context)
 
-
-def detail_view(request,post_id):
+def detail_view(request, post_id):
     published = Post.objects.exclude(published_date__exact=None)
     try:
         post = published.get(pk=post_id)
     except Post.DoesNotExist:
         raise Http404
     context = {'post': post}
-    #render completes template loading, creating context, generating page body, and making HTTP response at once
     return render(request, 'blogging/detail.html', context)
